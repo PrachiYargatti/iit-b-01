cbbbc5310d740cd765a8a5bc2e3e710a975e5032
Prachi Yaragatti <yaragattiprachi@gmail.com>
2025-12-10 19:34:45 +0530

    add subtract

---
+++ arithmetic.py
@@ -0,0 +1,6 @@
+
+def add(a,b):
+    print("addition : ", a+b)
+
+def subtract(a,b):
+    print("subtraction : ", a-b)
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import arithmetic
+
 print("hello world")
+
+num1 = int(input("enter num1 : "))
+num2 = int(input("enter num2 : "))
+arithmetic.add(num1,num2)
+arithmetic.subtract(num1,num2)
